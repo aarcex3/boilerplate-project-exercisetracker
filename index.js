7ccc61d56391c6fff6126d5763b3ab982a44c0dd
Agustin Arce <agustinarceprincich@gmail.com>
2024-04-26 20:49:06 +0000

    Passed 6/8 tests

--- index.js
+++ index.js
@@ -1,86 +1,89 @@
-const express = require('express')
+const express = require('express');
 const crypto = require('crypto');
-const app = express()
 const cors = require('cors');
-const { log, count } = require('console');
-require('dotenv').config()
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
-app.use(express.static('public'))
-
+app.use(cors());
+app.use(express.static('public'));
 
 function generateHashId(username) {
   const hashId = crypto.createHash('md5');
   hashId.update(Buffer.from(username));
-  return hashId.digest('hex').substring(0,8);
+  return hashId.digest('hex').substring(0, 8);
 }
 
-
-// Tables
-const users = {}
-const logs = {}
-
-
+const users = {};
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/api/users',(req,res)=>{
-  let arrayOfUsers = []
-  for (var id in users) {
-      arrayOfUsers.push({username:users[id],_id:id})
-    }
-    res.json(arrayOfUsers)
+app.get('/api/users', (req, res) => {
+  const arrayOfUsers = Object.keys(users).map(id => ({
+    username: users[id].username,
+    _id: id
+  }));
+  res.status(200).json(arrayOfUsers);
 });
 
-app.post('/api/users',(req,res)=>{
+app.post('/api/users', (req, res) => {
   const { username } = req.body;
-  if (!username){
-    res.status(400).json({ error: 'Username is required' });
-  } 
-  const _id = generateHashId(username)
-  users[_id] = username
-  res.json({
-    username:username,
-    _id:_id
-  })
+  if (!username) {
+    return res.status(400).json({ error: 'Username is required' });
+  }
+
+  const _id = generateHashId(username);
+  const user = {
+    username,
+    count: 0,
+    _id,
+    log: []
+  };
+  
+  users[_id] = user;
+
+  res.status(200).json({
+    username,
+    _id
+  });
 });
 
-app.post('/api/users/:_id/exercises',(req,res)=>{
-   const { _id, description, duration} = req.body;
-   if (!_id || !description || !duration) {
+app.post('/api/users/:_id/exercises', (req, res) => {
+  const { _id } = req.params;
+  const { description, date } = req.body;
+  let { duration } = req.body;
+  if (!_id || !description || !duration) {
     return res.status(400).json({ error: "Please provide _id, description, and duration." });
   }
-   let { date } = req.body;
-   date = !date ? new Date().toDateString() : new Date().toDateString(date);
-   const exercise = {
-    username:users[_id], 
-    description:description,
-    duration:duration,
-    date: date,
-    _id: _id
-  }
-  if(logs[_id]){
-    logs[_id].push(exercise)
-  } else {
-    logs[_id] = [exercise]
-  }
-  res.json(exercise);
-})
+  const parsedDate = Number(duration)
+  const exercise = {
+    description,
+    duration:parsedDate,
+    date: date || new Date().toDateString()
+  };
+  
+  users[_id].log.push(exercise);
+  users[_id].count++;
+  
+  res.json({
+    _id,
+    username: users[_id].username,
+    description,
+    parsedDate,
+    date: exercise.date
+  });
+});
 
-app.get('/api/users/:_id/logs',(req,res)=>{
-    const {_id} = req.params;
-    res.json({
-      username:users[_id],
-      count: logs[_id].length,
-      _id: _id,
-      log: logs[_id]
-    })
-})
+app.get('/api/users/:_id/logs', (req, res) => {
+  const { _id } = req.params;
+  res.status(200).json(users[_id]);
+});
 
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+const PORT = process.env.PORT || 3000;
+const listener = app.listen(PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
+});
